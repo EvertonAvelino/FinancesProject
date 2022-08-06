fd12ba2b78d7e373fab551961d43c264de84c005
Everton <56169155+EvertonAvelino@users.noreply.github.com>
2022-08-06 14:56:14 -0300

    mostrando gastos

--- finances/src/service/firebaseConnection.js
+++ finances/src/service/firebaseConnection.js
@@ -4,14 +4,14 @@ import 'firebase/database';
 import 'firebase/storage';
 
 let firebaseConfig = {
-	apiKey: "AIzaSyC2CItD2dHZybho2RBQItV8RergMDlsQdg",
-	authDomain: "finances-38a6f.firebaseapp.com",
-	databaseURL: "https://finances-38a6f-default-rtdb.firebaseio.com",
-	projectId: "finances-38a6f",
-	storageBucket: "finances-38a6f.appspot.com",
-	messagingSenderId: "567705842647",
-	appId: "1:567705842647:web:4a73a62e7768471b289192",
-	measurementId: "G-PW1VQ4J06M"
+	apiKey: "AIzaSyBcA9mb1y5IGJ9axndXKlNAbg3TIYoLAtE",
+	authDomain: "finances-66ebe.firebaseapp.com",
+	databaseURL: "https://finances-66ebe-default-rtdb.firebaseio.com",
+	projectId: "finances-66ebe",
+	storageBucket: "finances-66ebe.appspot.com",
+	messagingSenderId: "834680903565",
+	appId: "1:834680903565:web:cacf12e3582a0b539c0fc4",
+	measurementId: "G-H2PV9QNTQS"
 };
 
 if (!firebase.apps.length) {
